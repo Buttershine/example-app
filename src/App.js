13e7486d4eae96f911667500b32a1936ae07e734
Buttershine <gaedk003@gmail.com>
2017-12-09 21:54:50 -0600

    added filter for price

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import {getTicker} from './utility/httputil.js';
 import TickerPanel from './tickerPanel/tickerPanel.jsx';
+import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,18 +14,26 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My React Application</h1>
+          <h1 className="App-title">Some Awesome Crypto</h1>
         </header>
+        <div class="row">
+          <div class="pull-left button-panel">
+            <DropdownButton title="Percentage Change" onSelect={this.onItemClick}>
+              <MenuItem eventKey={1}>1h</MenuItem>
+              <MenuItem eventKey={2}>24h</MenuItem>
+              <MenuItem eventKey={3}>7d</MenuItem>
+            </DropdownButton>
+          </div>
+        </div>
         <div class="row">
           <div>
             <div class="header">
             </div>
             <div class="panel panel-default">
-            <TickerPanel data = {this.state.data}/>
+            <TickerPanel data = {this.state.priceList}/>
             </div>
           </div>
         </div>
-        <button onClick={this.handleClick}>Get Prices</button>
       </div>
     );
   }
@@ -31,17 +41,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      items: [],
-        prevState: {}
+      prevState: {},
+      priceList: []
     };
   }
 
-  componentDidMount() {
+  getInitialState() {
+    return {
+      name: this.props.name
+    };
+  }
+
+  componentDidMount() { //autoruns
     var _this = this;
     getTicker('https://api.coinmarketcap.com/v1/ticker/')
     .then(function(response){
       _this.setState({
-        data: response.data
+        priceList: response.data //assigns the array to the priceList object
       });
     })
     .catch(function(e) {
@@ -49,11 +65,26 @@ class App extends Component {
     })
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  };
+  onItemClick = (evt) => {
+    if(evt === 1) {
+      var temp = this.state.priceList.sort(function(a, b) {
+        return parseFloat(b.percent_change_1h) - parseFloat(a.percent_change_1h);    
+      });
+    } else if(evt === 2) {
+      var temp = this.state.priceList.sort(function(a, b) {
+      return parseFloat(b.percent_change_24h) - parseFloat(a.percent_change_24h);
+      });
+    } else if(evt === 3) {
+      var temp = this.state.priceList.sort(function(a, b) {
+      return parseFloat(b.percent_change_7d) - parseFloat(a.percent_change_7d);
+      });
+    }
+
+      this.setState({
+        priceList: temp //assigns the array to the priceList object
+      });
+
+  }
 
 }
 
---
+++ src/dropdowns/priceDropdown.js
@@ -0,0 +1,5 @@
+            <DropdownButton title="Percentage Change" onSelect={this.onItemClick}>
+              <MenuItem eventKey={1}>1h</MenuItem>
+              <MenuItem eventKey={2}>24h</MenuItem>
+              <MenuItem eventKey={3}>7d</MenuItem>
+            </DropdownButton>
